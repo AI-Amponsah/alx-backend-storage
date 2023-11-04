5fc5e155dd0640dded5491bd4cdf4385f27a7c3e
AI-Amponsah <Yawamp27@gmail.com>
2023-11-04 18:49:25 +0000

    version

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python3
-
-'''
-Improve 12-log_stats.py by adding the top 10 of the most present IPs in the
-collection nginx of the database logs:
-'''
-
+""" Log stats - new version """
 from pymongo import MongoClient
 
 
 def nginx_stats_check():
-    '''
-    Nginx function
-    '''
+    """ provides some stats about Nginx logs stored in MongoDB:"""
     client = MongoClient()
     collection = client.logs.nginx
 
-    number_of_documents = collection.count_documents({})
-    print("{} logs".format(number_of_documents))
+    num_of_docs = collection.count_documents({})
+    print("{} logs".format(num_of_docs))
     print("Methods:")
-
     methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods_list:
         method_count = collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, method_count))
-
     status = collection.count_documents({"method": "GET", "path": "/status"})
-
     print("{} status check".format(status))
+
     print("IPs:")
 
     top_IPs = collection.aggregate([
-        {"$group": {
-            "_id": "$ip",
-            "count": {"$sum": 1}
-        }},
+        {"$group":
+         {
+             "_id": "$ip",
+             "count": {"$sum": 1}
+         }
+         },
         {"$sort": {"count": -1}},
         {"$limit": 10},
         {"$project": {
@@ -42,11 +35,9 @@ def nginx_stats_check():
             "count": 1
         }}
     ])
-
     for top_ip in top_IPs:
         count = top_ip.get("count")
         ip_address = top_ip.get("ip")
-
         print("\t{}: {}".format(ip_address, count))
 
 
